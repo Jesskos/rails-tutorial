a7532b390489508689542e94f9f915796b708b50
Jessica Koscheka <jesskos@gmail.com>
2018-08-24 15:03:04 -0700

    took notes from tutorial below example_user class

--- sample_app/environment/sample_app/example_user.rb
+++ sample_app/environment/sample_app/example_user.rb
@@ -1,6 +1,8 @@
 
 class User
+
     attr_accessor :name, :email
+    
     
     def initialize(attributes = {})
         @name  = attributes[:name]
@@ -11,3 +13,8 @@ class User
         "#{@name} <#{@email}>"
     end
 end
+
+# creates getter and setter methods to allow us to retrieve get and assign set@name and @email instance variables
+# instance variables always begin with an @ sign
+# initialize method is a specialized Ruby method, called when we execute User.new
+# require is used to load example user code
