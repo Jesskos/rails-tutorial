0cb3d03ecf6ae2cf4ff987338da620e5c0b453b8
Jessica Koscheka <jesskos@gmail.com>
2018-09-02 20:24:35 -0700

    added a password and its confirmatioan in tests

--- sample_app/environment/sample_app/test/models/user_test.rb
+++ sample_app/environment/sample_app/test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-      @user = User.new(name: "Example User", email: "user@gmail.com")
+      @user = User.new(name: "Example User", email: "user@example.com",
+                       password: "foobar", password_confirmation: "foobar")
     end
   
   test "should be valid" do
@@ -50,4 +51,6 @@ class UserTest < ActiveSupport::TestCase
         @user.save
         assert_equal mixed_case_email.downcase, @user.reload.email
     end
+
+
 end
