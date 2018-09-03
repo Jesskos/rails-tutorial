3fdd34e89ed9802aec67f064e4b6444f75dc7101
Jessica Koscheka <jesskos@gmail.com>
2018-09-02 19:35:33 -0700

    performed migration to add a password_digest column

--- sample_app/environment/sample_app/db/schema.rb
+++ sample_app/environment/sample_app/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180902014943) do
+ActiveRecord::Schema.define(version: 20180903023102) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
