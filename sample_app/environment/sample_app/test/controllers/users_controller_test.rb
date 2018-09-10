ca4bc17ba813f34ba32a24e71f08b3ebde688815
Jessica Koscheka <jesskos@gmail.com>
2018-09-10 14:15:36 -0400

    added test to make sure admin attribute is forbidden

--- sample_app/environment/sample_app/test/controllers/users_controller_test.rb
+++ sample_app/environment/sample_app/test/controllers/users_controller_test.rb
@@ -27,6 +27,23 @@ test "should redirect update when logged in as wrong user" do
     assert_redirected_to root_url
 end
 
+test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+        email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+end
+
+test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+        user: { password:              'password',
+            password_confirmation: 'password',
+            admin: true } }
+    assert_not @other_user.reload.admin?
+end
+
 test "should redirect index when not logged in" do
     get users_path
     assert_redirected_to login_url
