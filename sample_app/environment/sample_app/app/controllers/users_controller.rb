46da18947ddfca1acffc684b6a44ec5865b15711
Jessica Koscheka <jesskos@gmail.com>
2018-09-09 23:59:57 -0400

    added index to controller

--- sample_app/environment/sample_app/app/controllers/users_controller.rb
+++ sample_app/environment/sample_app/app/controllers/users_controller.rb
@@ -4,7 +4,9 @@ class UsersController < ApplicationController
       before_action :logged_in_user, only: [:edit, :update]
       before_action :correct_user,   only: [:edit, :update]
 
-    
+
+    def index
+    end
     
     def new
         @user = User.new
